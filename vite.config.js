146b3cda766fb4e164a65dcc8984ef8dbece9cd4
hectorLandaez <143516248+hectorLandaez@users.noreply.github.com>
2023-10-18 02:04:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pokedex_dia15"
+  base: "https://hectorLandaez/deploypoke"
 })
